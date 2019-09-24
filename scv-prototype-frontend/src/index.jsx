b21d92591f38f945bda1ae853c0d3b1b38ba0da3
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2019-09-24 11:23:03 -0230

    Remove react-router from index.jsx

--- scv-prototype-frontend/src/index.jsx
+++ scv-prototype-frontend/src/index.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './app/components/App';
 import './app/i18n';
 import * as serviceWorker from './service-worker';
 
-ReactDOM.render(<Router basename={`${process.env.PUBLIC_URL}`}><App /></Router>, document.querySelector('AppRoot'));
+ReactDOM.render(<App />, document.querySelector('AppRoot'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
