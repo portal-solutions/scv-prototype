ca17a68f7150362a41ab1179fb3292c0c410bcba
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2019-09-25 12:07:54 -0230

    Re-order imports in index.jsx for IE polyfills

--- scv-prototype-frontend/src/index.jsx
+++ scv-prototype-frontend/src/index.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import './app/i18n';
