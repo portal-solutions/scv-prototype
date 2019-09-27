860b5fd0cd6f3cb61544a2afe30d9ec974a2a816
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2019-09-27 09:58:03 -0230

    Changed promises to async/await for cleaner code

--- scv-prototype-frontend/src/app/services/ApiService.jsx
+++ scv-prototype-frontend/src/app/services/ApiService.jsx
@@ -5,33 +5,35 @@ const apiService = {
 	/**
 	 * Perform a login by POSTing a username and password to the API.
 	 */
-	login: (username, password) => {
-		return fetch(baseUrl + '/auth', {
+	login: async (username, password) => {
+		const response = await fetch(baseUrl + '/auth', {
 			method: 'POST', mode: 'cors', cache: 'no-cache',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({ username, password })
 		})
-		.then((response) => {
-			if (response.ok) { return response; }
-			throw new Error('Authentication failed; response status: ' + response.status);
-		})
-		.then((response) => response.json());
+
+		if (!response.ok) {
+			throw new Error('Error fetching greetings; response status: ' + response.status);
+		}
+
+		return await response.json();
 	},
 
 	/**
 	 * Fetch some greetings.
 	 * TODO :: GjB :: remove this eventually
 	 */
-	fetchGreetings: (authToken) => {
-		return fetch(baseUrl + '/api/greetings', {
+	fetchGreetings: async (authToken) => {
+		const response = await fetch(baseUrl + '/api/greetings', {
 			method: 'GET', mode: 'cors', cache: 'no-cache',
 			headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken }
-		})
-		.then((response) => {
-			if (response.ok) { return response; }
+		});
+
+		if (!response.ok) {
 			throw new Error('Error fetching greetings; response status: ' + response.status);
-		})
-		.then((response) => response.json());
+		}
+
+		return await response.json();
 	}
 };
 
