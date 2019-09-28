077553061128bdec6fa10e47878926efb83441aa
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2019-09-28 11:26:51 -0230

    Update ApiService to use env configs (+ minor cleanup)

--- scv-prototype-frontend/src/app/services/ApiService.jsx
+++ scv-prototype-frontend/src/app/services/ApiService.jsx
@@ -1,19 +1,24 @@
-// TODO :: GjB :: make this configurable
-const baseUrl = 'https://scv-prototype-api.azurewebsites.net';
+import config from '../../config';
 
+const { baseUrl } = config.api;
+
+/**
+ * A one-stop shop for all things API related.
+ */
 const apiService = {
+
 	/**
 	 * Perform a login by POSTing a username and password to the API.
 	 */
 	login: async (username, password) => {
-		const response = await fetch(baseUrl + '/auth', {
+		const response = await fetch(`${baseUrl}/auth`, {
 			method: 'POST', mode: 'cors', cache: 'no-cache',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({ username, password })
 		})
 
 		if (!response.ok) {
-			throw new Error('Error fetching greetings; response status: ' + response.status);
+			throw new Error('Error acquiring JWT token; response status: ' + response.status);
 		}
 
 		return await response.json();
@@ -24,9 +29,9 @@ const apiService = {
 	 * TODO :: GjB :: remove this eventually
 	 */
 	fetchGreetings: async (authToken) => {
-		const response = await fetch(baseUrl + '/api/greetings', {
+		const response = await fetch(`${baseUrl}/api/greetings`, {
 			method: 'GET', mode: 'cors', cache: 'no-cache',
-			headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken }
+			headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${authToken}` }
 		});
 
 		if (!response.ok) {
@@ -38,12 +43,11 @@ const apiService = {
 
 	/**
 	 * Fetch user profile.
-	 * TODO :: GjB :: remove this eventually
 	 */
 	fetchProfile: async (authToken, id) => {
 		const response = await fetch(`${baseUrl}/api/profiles/${id}`, {
 			method: 'GET', mode: 'cors', cache: 'no-cache',
-			headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + authToken }
+			headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${authToken}` }
 		});
 
 		if (!response.ok) {
