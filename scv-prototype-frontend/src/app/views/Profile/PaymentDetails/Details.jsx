94203630ce98ae19ce095ebe22cff0433f699474
Sebastien Comeau <sebascomeau@gmail.com>
2019-10-01 14:05:13 -0300

    Minor teaks based on react console warning.

--- scv-prototype-frontend/src/app/views/Profile/PaymentDetails/Details.jsx
+++ scv-prototype-frontend/src/app/views/Profile/PaymentDetails/Details.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
+import Loading from '../../../components/Loading';
 
 const Details = () => {
 
@@ -14,7 +15,7 @@ const Details = () => {
 			image: imageUrl
 		},
 		{
-			id: 1,
+			id: 2,
 			title: "Bank of Montreal",
 			identifier: "********8563",
 			usedFor: ["Canada Learning Bond"],
@@ -38,54 +39,88 @@ const Details = () => {
 
 	const { t } = useTranslation();
 
+	const [data, setData] = useState(null);
+	const [isLoading, setIsLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+
+	useEffect(() => {
+		const fetchHistory = async () => {
+			setIsError(false);
+			setIsLoading(true);
+
+			try {
+				// simulate fake fetch with sleep
+				await (new Promise((resolve) => setTimeout(resolve, 1000)));
+
+				//set data with fake data
+				setData(fakeData);
+			} catch (error) {
+				console.error(error)
+				setIsError(true);
+			}
+
+			setIsLoading(false);
+		}
+
+		fetchHistory();
+	}, []);
+
 	return (
 		<>
 			<div className="row">
-				<div className="col-xs-12">
-					<div className="panel panel-default">
-						{fakeData && fakeData.length ?
-							<ul className="list-group">
-								{fakeData.map(item =>
-									<li className="list-group-item" key={item.id}>
-										<div className="media">
-											<div className="media-left">
-												<img className="media-object" src={item.image} alt={item.title} />
-											</div>
-											<div className="media-body">
-												<h4 className="media-heading">{item.title}</h4>
-												<p>{item.identifier}</p>
-												<p>
-													{
-														item.usedFor ?
-															<Trans i18nKey="profile.payment-details.details.used-for">
-																{
-																	item.usedFor.join(', ')
-																}
-															</Trans> :
-															<Trans i18nKey="profile.payment-details.details.not-in-use" />
-													}
-												</p>
-												<div>
-													<button className="btn btn-link btn-sm">{t('action.edit')}</button>
-													<small>|</small>
-													<button className="btn btn-link btn-sm">{t('action.remove')}</button>
-												</div>
-											</div>
-										</div>
-
-									</li>
-								)}
-							</ul>
-
-							:
-							<div className="panel-body">
-								<p className="text-center"><em>{t('no-data-available')}</em></p>
-							</div>
-						}
-
-					</div>
+				<div className="col-xs-12 text-center">
+					{isLoading && <div className="text-center"><Loading /></div>}
+					{isError && <h4 className="text-center">{t('something-went-wrong')}</h4>}
 				</div>
 			</div>
+
+			{(!isLoading && !isError) &&
+				<div className="row">
+					<div className="col-xs-12">
+						<div className="panel panel-default">
+							{fakeData && fakeData.length ?
+								<ul className="list-group">
+									{fakeData.map(item =>
+										<li className="list-group-item" key={item.id}>
+											<div className="media">
+												<div className="media-left">
+													<img className="media-object" src={item.image} alt={item.title} />
+												</div>
+												<div className="media-body">
+													<h4 className="media-heading">{item.title}</h4>
+													<p>{item.identifier}</p>
+													<p>
+														{
+															item.usedFor ?
+																<Trans i18nKey="profile.payment-details.details.used-for">
+																	{
+																		item.usedFor.join(', ')
+																	}
+																</Trans> :
+																<Trans i18nKey="profile.payment-details.details.not-in-use" />
+														}
+													</p>
+													<div>
+														<button className="btn btn-link btn-sm">{t('action.edit')}</button>
+														<small>|</small>
+														<button className="btn btn-link btn-sm">{t('action.remove')}</button>
+													</div>
+												</div>
+											</div>
+
+										</li>
+									)}
+								</ul>
+
+								:
+								<div className="panel-body">
+									<p className="text-center"><em>{t('no-data-available')}</em></p>
+								</div>
+							}
+
+						</div>
+					</div>
+				</div>}
 		</>
 	);
 };
