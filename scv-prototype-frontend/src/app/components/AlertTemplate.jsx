9b2b18a65125062da8fbe7cbaecd3dd3cd00ef7f
unknown <SEBASTIEN.COMEAU@HRSDC-RHDCC.GC.CA>
2019-10-10 10:01:43 -0300

    Fixed alert template style.

--- scv-prototype-frontend/src/app/components/AlertTemplate.jsx
+++ scv-prototype-frontend/src/app/components/AlertTemplate.jsx
@@ -5,22 +5,35 @@ const alertStyle = {
   backgroundColor: '#151515',
   color: 'white',
   padding: '10px',
-  textTransform: 'uppercase',
   borderRadius: '3px',
   display: 'flex',
-  justifyContent: 'space-between',
   alignItems: 'center',
+  justifyContent: 'space-between',
   boxShadow: '0px 2px 2px 2px rgba(0, 0, 0, 0.03)',
   width: '300px',
-  boxSizing: 'border-box'
+  boxSizing: 'border-box',
+  fontSize: '0.85em',
+  fontFamily: 'Arial'
 };
 
 const buttonStyle = {
-  marginLeft: '20px',
+  marginLeft: '10px',
+  padding: '0',
+  flexGrow: '1',
   border: 'none',
   backgroundColor: 'transparent',
   cursor: 'pointer',
-  color: '#FFFFFF'
+  color: '#FFFFFF',
+  alignSelf: 'flex-start'
+};
+
+const messageStyle = {
+  width: '100%'
+};
+
+const iconContainerStyle = {
+  marginRight: '5px',
+  fontSize: '2em'
 };
 
 const AlertTemplate = ({ message, options, style, close }) => {
@@ -29,19 +42,19 @@ const AlertTemplate = ({ message, options, style, close }) => {
   const icons = {
     info: {
       title: t('alert.info'),
-      iconClassName: 'fas fa-info text-info'
+      iconClassName: 'fas fa-info-circle fa-fw text-info'
     },
     success: {
       title: t('alert.success'),
-      iconClassName: 'fas fa-success text-success'
+      iconClassName: 'fas fa-check-circle fa-fw text-success'
     },
     error: {
       title: t('alert.error'),
-      iconClassName: 'fas fa-error text-error'
+      iconClassName: 'fas fa-exclamation-circle fa-fw text-danger'
     },
     close: {
       title: t('alert.close'),
-      iconClassName: 'fas fa-times'
+      iconClassName: 'fas fa-window-close'
     }
   };
 
@@ -59,15 +72,15 @@ const AlertTemplate = ({ message, options, style, close }) => {
   return (
     <div style={{ ...alertStyle, ...style }}>
       {alertTypeIcon && (
-        <span>
+        <span style={iconContainerStyle}>
           <span className="sr-only">{alertTypeIcon.title}</span>
           <i className={alertTypeIcon.iconClassName}></i>
         </span>
       )}
-      <span style={{ flex: 2 }}>{message}</span>
-      <button onClick={close} style={buttonStyle}>
-        <span className="sr-only">{icons.error.title}</span>
-        <i className={icons.error.iconClassName}></i>
+      <span style={messageStyle}>{message}</span>
+      <button type="button" onClick={close} style={buttonStyle}>
+        <span className="sr-only">{icons.close.title}</span>
+        <i className={icons.close.iconClassName}></i>
       </button>
     </div>
   );
