24ef22f0439eede49626fb261930bf1e78068e16
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2019-10-10 11:21:03 -0230

    Cleanup of App.jsx

--- scv-prototype-frontend/src/app/App.jsx
+++ scv-prototype-frontend/src/app/App.jsx
@@ -1,19 +1,17 @@
 import { createBrowserHistory } from 'history';
 import React from 'react';
+import { Provider as AlertProvider } from 'react-alert';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 import './App.scss';
+import AlertTemplate from './components/AlertTemplate';
 import Error404 from './components/error/Error404';
 import PrivateRoute from './components/PrivateRoute';
 import MainLayout from './layout/Main';
 import { ApiProvider } from './utils/api';
 import { AuthProvider } from './utils/auth';
 import { PageMetadataProvider } from './utils/page-metadata';
-import PrivateRoute from './components/PrivateRoute';
-import MainLayout from './layout/Main';
 import Login from './views/Login/Login';
 import Private from './views/Private';
-import Error404 from './components/error/Error404';
-import './App.scss';
 
 // alert optional cofiguration
 const alertOptions = {
@@ -36,23 +34,22 @@ const App = () => {
     // eslint-disable-next-line react/jsx-props-no-spreading
     <AlertProvider template={AlertTemplate} {...alertOptions}>
       <AuthProvider>
-      <ApiProvider>
-
-        <PageMetadataProvider>
-          <Router history={history}>
-            <MainLayout>
-              <Switch>
-                <Route path="/" exact>
-                  <Redirect to="/private" />
-                </Route>
-                <Route path="/sign-in" component={Login} exact />
-                <PrivateRoute path="/private" component={Private} />
-                <Route path="*" component={Error404} />
-              </Switch>
-            </MainLayout>
-          </Router>
-        </PageMetadataProvider>
-</ApiProvider>
+        <ApiProvider>
+          <PageMetadataProvider>
+            <Router history={history}>
+              <MainLayout>
+                <Switch>
+                  <Route path="/" exact>
+                    <Redirect to="/private" />
+                  </Route>
+                  <Route path="/sign-in" component={Login} exact />
+                  <PrivateRoute path="/private" component={Private} />
+                  <Route path="*" component={Error404} />
+                </Switch>
+              </MainLayout>
+            </Router>
+          </PageMetadataProvider>
+        </ApiProvider>
       </AuthProvider>
     </AlertProvider>
   );
