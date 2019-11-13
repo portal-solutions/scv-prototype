62344808db81ab8f249e6c3d88e41f4d9c2bddac
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2019-11-13 12:27:10 -0330

    Update interop health indicator

---
+++ scv-prototype-api/src/main/java/ca/gov/portal/scv/api/service/dto/OpenApiInfo.java
@@ -0,0 +1,27 @@
+package ca.gov.portal.scv.api.service.dto;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * @author Greg Baker (gregory.j.baker@hrsdc-rhdcc.gc.ca)
+ * @since 0.0.0
+ */
+@Value
+@Builder
+@SuppressWarnings({ "serial" })
+@JsonDeserialize(builder = OpenApiInfo.OpenApiInfoBuilder.class)
+public class OpenApiInfo implements Serializable {
+
+	@JsonProperty("title")
+	private String title;
+
+	@JsonProperty("version")
+	private String version;
+
+}
