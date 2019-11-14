dd6aad89bbac1d52b08ee0ef9fd5effded67a962
Rui Wang <rui.wang@hrsdc-rhdcc.gc.ca>
2019-11-14 11:45:01 -0500

    Added service for getLocation

--- scv-prototype-api/src/main/java/ca/gov/portal/scv/api/service/InteropService.java
+++ scv-prototype-api/src/main/java/ca/gov/portal/scv/api/service/InteropService.java
@@ -1,6 +1,7 @@
 package ca.gov.portal.scv.api.service;
 
 import ca.gov.portal.scv.api.service.dto.OpenApiInfo;
+import ca.gov.portal.scv.api.service.dto.Location;
 
 /**
  * @author Greg Baker (gregory.j.baker@hrsdc-rhdcc.gc.ca)
@@ -10,4 +11,5 @@ public interface InteropService {
 
 	OpenApiInfo isAvailable();
 
+	Location getLocation(String addressStr);
 }
