5673da705ea5a92e1625baf43ff954d16a1c79ba
Sebastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2019-11-15 16:00:25 -0400

    Implementing modal.

--- scv-prototype-frontend/src/app/views/Private/Profile/Addresses/Addresses.jsx
+++ scv-prototype-frontend/src/app/views/Private/Profile/Addresses/Addresses.jsx
@@ -1,35 +1,51 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 import Address from './Address';
 import Button from '../../../../components/Button';
+import Modal from '../../../../components/ui/Modal/Modal';
 
 const Addresses = ({ addresses }) => {
   const { t } = useTranslation();
 
+  const [addAddress, setAddAddress] = useState(false);
+
+  const addAdress_OnClose = () => {
+    setAddAddress(false);
+  };
+
+  const addAdress_OnSubmit = () => {
+    setAddAddress(false);
+  };
+
   return (
-    <div className="profile__section">
-      <div className="profile__section__icon">
-        <i className="fas fa-map-marker-alt fa-fw" />
-      </div>
-      <div className="profile__section__content">
-        <div className="profile__section__content__title">
-          <span>{t('private.profile.addresses.title')}</span>
-          <Button size={Button.sizes.default} variant={Button.variants.default}>
-            <i className="fas fa-plus" />
-            {t('private.profile.addresses.add')}
-          </Button>
+    <>
+      <div className="profile__section">
+        <div className="profile__section__icon">
+          <i className="fas fa-map-marker-alt fa-fw" />
         </div>
-        {addresses && addresses.length ? (
-          addresses.map((a, i) => <Address key={i} program={a.program} address={a.address} />)
-        ) : (
-          <p className="text-center">
-            <em>{t('no-data-available')}</em>
-          </p>
-        )}
+        <div className="profile__section__content">
+          <div className="profile__section__content__title">
+            <span>{t('private.profile.addresses.title')}</span>
+            <Button size={Button.sizes.default} variant={Button.variants.default} onClick={() => setAddAddress(true)}>
+              <i className="fas fa-plus" />
+              {t('private.profile.addresses.add')}
+            </Button>
+          </div>
+          {addresses && addresses.length ? (
+            addresses.map((a, i) => <Address key={i} program={a.program} address={a.address} />)
+          ) : (
+            <p className="text-center">
+              <em>{t('no-data-available')}</em>
+            </p>
+          )}
+        </div>
       </div>
-    </div>
+      <Modal title="Add an address" show={addAddress} onClose={addAdress_OnClose} onSubmit={addAdress_OnSubmit}>
+        test mon copain
+      </Modal>
+    </>
   );
 };
 
