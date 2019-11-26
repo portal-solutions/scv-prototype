7c465fb370c6d6cd676ea5c9ea366233e2ec599c
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2019-11-26 11:29:21 -0330

    Refactor locations endpoint; add controller DTO

---
+++ scv-prototype-api/src/main/java/ca/gov/portal/scv/api/service/dto/LocationResponse.java
@@ -0,0 +1,24 @@
+package ca.gov.portal.scv.api.service.dto;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * @author Greg Baker (gregory.j.baker@hrsdc-rhdcc.gc.ca)
+ * @since 0.0.0
+ */
+@Value
+@Builder
+@SuppressWarnings({ "serial" })
+@JsonDeserialize(builder = LocationResponse.LocationResponseBuilder.class)
+public class LocationResponse implements Serializable {
+
+	@JsonProperty("Location")
+	private Location location;
+
+}
